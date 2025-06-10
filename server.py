a8d84850dd2f9781547bfb9f8d9fe3c675bf2806
leun <luokun@zecaimao.cn>
2025-06-10 22:54:09 +0800

    feat(server): 修改服务器监听地址为0.0.0.0

--- server.py
+++ server.py
@@ -19,4 +19,4 @@ app.register_blueprint(api, url_prefix='/api')
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=True)
